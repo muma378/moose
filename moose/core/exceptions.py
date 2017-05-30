5c8f93a7ae6908feb8cd0f3379a9c2ad4dcfa22d
xiaoyang <muma.378@163.com>
2017-05-30 21:01:07 +0800

    [ADD] exceptions for moose.core

---
+++ moose/core/exceptions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+class BaseException(Exception):
+	def __init__(self, msg):
+		super(BaseException, self).__init__()
+		self.msg = msg
+
+
+class ArgumentsError(BaseException):
+	def __init__(self, msg='invalid arguments passed in'):
+		super(ArgumentsError, self).__init__(msg)
+
+class ConnTimeoutError(BaseException):
+	def __init__(self, msg='connection timeout'):
+		super(ConnTimeoutError, self).__init__(msg)
+
+class ImproperlyConfigured(Exception):
+    """moose is somehow improperly configured"""
+    pass
+
+class AppRegistryNotReady(Exception):
+    """The moose.apps registry is not populated yet"""
+    pass
+
+class DoesNotExist(Exception):
+	"""File specified does not exist"""
+	pass
