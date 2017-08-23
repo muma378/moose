22c3e9059dc6be84c332e9c59ab20ab1f80e1dae
xiaoyang <muma.378@163.com>
2017-08-23 21:42:30 +0800

    [FIX] add exceptions of SuspiciousOperations

--- moose/core/exceptions.py
+++ moose/core/exceptions.py
@@ -24,3 +24,10 @@ class AppRegistryNotReady(Exception):
 class DoesNotExist(Exception):
 	"""File specified does not exist"""
 	pass
+
+class SuspiciousOperation(Exception):
+    """The user did something suspicious"""
+
+class SuspiciousFileOperation(SuspiciousOperation):
+    """A Suspicious filesystem operation was attempted"""
+    pass
