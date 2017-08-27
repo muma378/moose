366b976e30a9c90ae4926500dd4fbd645cf9b5f6
Xiao Yang <muma.378@163.com>
2017-08-27 19:22:03 +0800

    [ADD] add function ivisit to enumerate files

---
+++ moose/shortcuts.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import os
+import fnmatch
+
+
+def ivisit(src, dst=None, pattern=None, ignorecase=True):
+    """
+    A iterator to find all files in path `src` and match the Unix shell-like
+    `pattern` if provided. The value of `pattern` could be a string or a tuple, and would
+    perform a case-sensitive comparision if `ignorecase` was set to False.
+
+    Details for unix shell-like wildcards can be seen from:
+        https://docs.python.org/2/library/fnmatch.html#module-fnmatch
+
+    If `dst` was provided, returns a pair consists of source path and
+    destination with sub-directories. For example, if `src` was set to '/a'
+    `dst` was set to '/b', while a file was located at '/a/c/d.txt', will
+    yield a tuple ('/a/c/d.txt', '/b/c/d.txt')
+    """
+    # syntax sugar, converts string to a tuple with one element
+    if isinstance(pattern, str):
+        pattern = (pattern, )
+
+    # defines the function anymatch
+    if pattern:
+        if ignorecase:
+            pattern = [p.lower() for p in pattern]
+            matchfn = lambda s, p: fnmatch.fnmatch(s.lower(), p)
+        else:
+            matchfn = fnmatch.fnmatch
+        # test if there was any one matched a pattern in the list
+        anymatch = lambda s, ps: any([matchfn(s, p) for p in ps])
+    else:
+        # returns True always
+        anymatch = lambda s, ps: True
+
+    # traverse the source path
+    for dirpath, dirnames, filenames in os.walk(src):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            if anymatch(filepath, pattern):
+                # if `dst` was set, returns a pair
+                if dst:
+                    relpath = os.path.relpath(filepath, src)
+                    yield (filepath, os.path.join(dst, relpath))
+                else:
+                    yield filepath
---
+++ tests/test_utils/test_ivisit.py
@@ -0,0 +1,46 @@
+import unittest
+
+from moose.shortcuts import ivisit
+
+class IvisitTestCase(unittest.TestCase):
+    """
+    ivisit('src')
+        returns all files in path `src`
+
+    ivisit('src', pattern='*.txt', ignorecase=False)
+        returns all files matches the shell-like pattern `*.txt` in `src`,
+        performs a case-insensitive comparision
+
+    ivisit('src', 'dst')
+        returns all files in `src` meanwhile new path in `dst`.
+
+    """
+    def setUp(self):
+        self.test_data_path = "tests/sample_data/utils"
+
+    # a non-iterator version to ivisit
+    def visit(self, **kwargs):
+        filenames = []
+        for x in ivisit(self.test_data_path, **kwargs):
+            filenames.append(x)
+        return filenames
+
+    def test_visit_src(self):
+        self.assertEqual(len(self.visit()), 3)   # a.txt, B.txt, b/b.yaml
+        self.assertEqual(
+            len(self.visit(pattern="*.txt")), 2
+        )    # a.txt, B.txt
+        self.assertEqual(
+            self.visit(pattern="*b.*"),
+            ['tests/sample_data/utils/B.txt','tests/sample_data/utils/b/b.yaml',]
+        )
+        self.assertEqual(
+            self.visit(pattern="*b.*", ignorecase=False),
+            ['tests/sample_data/utils/b/b.yaml',]
+        )
+        self.assertEqual(
+            len(self.visit(pattern=("*.txt", "*b.*"))), 3
+        ) # a.txt, B.txt, b/b.yaml
+
+    def test_visit_dst(self):
+        self.assertEqual([p[1] for p in self.visit(dst='test')], ['test/a.txt', 'test/B.txt', 'test/b/b.yaml'])
