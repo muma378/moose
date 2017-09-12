e5cb27e90f514058794f96c5adcfd67d13ad69c4
Xiao Yang <muma.378@163.com>
2017-09-12 15:43:36 +0800

    [FIX] fix project demo

--- demo/cityscape/actions.py
+++ demo/cityscape/actions.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 import time
+import logging
 from moose import actions
+from moose.connection.cloud import azure
+
+logger = logging.getLogger('moose.actions')
 
 class Upload(actions.AbstractAction):
     def run(self, **kwargs):
         config = kwargs.get('config')
         time.sleep(2)
+        logger.info("upload files in '%s'" % config.common['root'])
         return "upload files in '%s'" % config.common['root']
 
 
