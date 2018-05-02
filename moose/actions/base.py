23ffb310733450ebecd4acf5ee1334b719e06736
Xiao Yang <muma.378@163.com>
2018-05-02 21:20:36 +0800

    [FIX] defines the base flow in actions

--- moose/actions/base.py
+++ moose/actions/base.py
@@ -3,9 +3,12 @@ import abc
 import threading
 
 class IllegalAction(Exception):
-	"""action was halted somehow"""
+	"""Action was halted somehow"""
 	pass
 
+class InvalidConfig(Exception):
+	"""Configs are not set as the requirement"""
+	pass
 
 class AbstractAction:
 	"""
@@ -25,6 +28,50 @@ class AbstractAction:
 	@abc.abstractmethod
 	def run(self, **kwargs):
 		raise NotImplementedError
+
+
+class BaseAction(AbstractAction):
+	"""
+	A standard base action is splited into 3 steps:
+
+	1. Get Environment
+		Converts options in *.cfg file to a dict named `environment`.
+		At this stage, you can keep arguments will be used in the
+		following steps and throw away ones won't.
+
+	2. Schedule
+		Splits and assembles the keys in `environment` to generate
+		a `context` as the argument to be called in the near future.
+		An `environment` may implicitly ask for a job done with
+		different context. At this stage, you are able to control how
+		they flow and the order to be handled.
+
+	3. Handle
+		Handles the job with the context provided above. Each handle()
+		should returns a `string` representing the result.
+	"""
+
+	def get_environment(self, kwargs):
+		raise NotImplementedError('subclasses of BaseAction must provide a get_environment()')
+
+	def schedule(self, environment):
+		raise NotImplementedError('subclasses of BaseAction must provide a schedule()')
+
+	def handle(self, context):
+		raise NotImplementedError('subclasses of BaseAction must provide a handle()')
+
+	def run(self, **kwargs):
+		environment = self.get_environment(kwargs)
+		output = []
+		for context in self.schedule(environment):
+			output.append(self.handle(context))
+		return '\n'.join(output)
+
+class BaseStat:
+	"""
+	An active counter to stash all statistic.
+	"""
+	pass
 
 
 class MixinAction(threading.Thread):
