521864ea8e89a798b63072e9ab404245349992a6
Xiao Yang <muma.378@163.com>
2018-09-26 01:23:24 +0800

    add unittest for mssql (fix #22)

---
+++ tests/test_db/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+mssql_settings = {
+    'HOST': '<host>',
+    'PORT': '<port>',
+    'USER': '<user>',
+    'PASSWORD': '<password>',
+    'DATABASE': '<database>',
+    'CHARSET': 'UTF-8',
+    'TABLE_ALIAS': {
+        'table_result': '[10.0.0.201].CrowdDB.dbo.DataResult',
+        'table_source': '[10.0.0.201].CrowdDB.dbo.DataSource',
+        'table_person': '[10.0.0.201].CrowdDB.dbo.Person',
+        'table_project': '[10.0.0.201].CrowdDB.dbo.Project',
+        'table_person_in_project': '[10.0.0.201].CrowdDB.dbo.PersonInProject',
+    },
+}
