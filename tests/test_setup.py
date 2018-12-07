cde90da8026be01395da78ed62ac6ab3184d71c5
Xiao Yang <muma.378@163.com>
2018-12-07 18:20:04 +0800

    add test case for moose.init

---
+++ tests/test_setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import mock
+import unittest
+import moose
+
+class SetupTestCase(unittest.TestCase):
+
+    def test_get_version(self):
+        # with mock.patch("moose") as mock_moose:
+        #     mock_moose.__file__ = u"/测试/路径/__init__.py".encode('gbk')
+        #     mock_moose.__file__ = u"/测试/路径/__init__.py"
+        #     moose.get_version()
+        pass
