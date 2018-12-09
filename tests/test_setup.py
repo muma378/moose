246144443843dea4e0f2f3e5a363aaa7e1a13397
Xiao Yang <muma.378@163.com>
2018-12-09 14:41:11 +0800

    add test data

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -3,17 +3,19 @@ import mock
 import unittest
 import moose
 
+import platform
+if platform.system()=="Windows":
+    npath = lambda x: x.replace('/', '\\').encode("gbk")
+else:
+    npath = lambda x: x.encode("utf-8")
+
 class SetupTestCase(unittest.TestCase):
 
     def test_get_version(self):
         self.assertRegexpMatches(moose.__version__, '[\d\.ab]+')
 
-        # won't raise UnicodeDecodeError
-        with self.assertRaises(IOError):
-            moose.__file__ = u"/测试/路径/__init__.py".encode('gbk')
-            moose.get_version()
-
-        moose.__file__ = u"tests/sample_data/setup/版本/__init__.py".encode('utf-8')
+        # if moose was installed under a path with chinese characters
+        moose.__file__ = npath(u"tests/sample_data/setup/版本/__init__.py")
         self.assertRegexpMatches(moose.get_version(), '[\d\.ab]+')
 
     def test_setup(self):
