af3ca2385cd83742e2cf76469279c08d2896f01c
Xiao Yang <muma.378@163.com>
2018-12-09 14:33:40 +0800

    add testcase for get_version

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -6,8 +6,16 @@ import moose
 class SetupTestCase(unittest.TestCase):
 
     def test_get_version(self):
-        # with mock.patch("moose") as mock_moose:
-        #     mock_moose.__file__ = u"/测试/路径/__init__.py".encode('gbk')
-        #     mock_moose.__file__ = u"/测试/路径/__init__.py"
-        #     moose.get_version()
-        pass
+        self.assertRegexpMatches(moose.__version__, '[\d\.ab]+')
+
+        # won't raise UnicodeDecodeError
+        with self.assertRaises(IOError):
+            moose.__file__ = u"/测试/路径/__init__.py".encode('gbk')
+            moose.get_version()
+
+        moose.__file__ = u"tests/sample_data/setup/版本/__init__.py".encode('utf-8')
+        self.assertRegexpMatches(moose.get_version(), '[\d\.ab]+')
+
+    def test_setup(self):
+        moose.setup()
+        # TODO: tests if logger and settings was configured
